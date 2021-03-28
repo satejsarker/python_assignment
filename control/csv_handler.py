247054f0284edf025eebff4573b37b4962ea0b87
satej <satej.sarker@iubh.de>
2021-03-28 23:11:03 +0600

    initial commit with common csv model

---
+++ control/csv_handler.py
@@ -0,0 +1,26 @@
+import pandas
+
+from model import session, ENGINE
+# from model.schema import Test
+from model.common import CommonCsvModel, Dataset
+
+# data = pandas.read_csv('../data/test.csv')
+# objs = []
+# for index, row in data.iterrows():
+#     objs.append(Test(row['x'], row['y']))
+#
+# session.bulk_save_objects(objects=objs)
+# session.commit()
+
+test_data = pandas.read_csv('../data/test.csv')
+train_data = pandas.read_csv('../data/train.csv')
+ideal_data = pandas.read_csv('../data/ideal.csv')
+
+# train data
+# TODO : create class for train data and move all the function to that class
+train_data_col = list(train_data.columns.values.tolist())
+# create table
+Train_table = CommonCsvModel(colums=train_data_col, data=train_data, engine=ENGINE, session=session)
+
+Train_table.create_table()
+Train_table.insert_data()
---
+++ control/train_data.py
@@ -0,0 +1,5 @@
+from model.common import CommonCsvModel
+
+
+class TrainData(CommonCsvModel):
+    pass
